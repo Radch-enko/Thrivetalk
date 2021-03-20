9bafe4c006926943496bcb73e4a98236bcd06d0e
Radch-enko <stas.radchenko.den@mail.ru>
2021-03-21 01:00:48 +0600

    Сделал десктопную версию

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-  alert();
+  $(".nav__item").click(function (e) {
+    $(".nav__item").removeClass("active");
+    $(this).toggleClass("active");
+  });
+  setHeigth();
+
+  $(window).resize(function () {
+    setHeigth();
+  });
 });
+
+function setHeigth() {
+  $(".can-help__square").css("height", $(".can-help__square").css("width"));
+  $(".big__square").css("height", $(".big__square").css("width"));
+}
